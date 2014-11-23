9436848ffb6a815080924f4bab372c2baeea481d
Johannes M. Schmitt <schmittjoh@gmail.com>
2014-11-24 00:13:27 +0100

    adds another method

--- src/Foo.php
+++ src/Foo.php
@@ -9,6 +9,7 @@ class Foo
     public function baz() { }
     public function abcdef() { }
     public function fiffieieueuezehejfjfj($a, $b, $c) { }
+    public function addsAnotherMethod() { }
 
     public function bar()
     {
